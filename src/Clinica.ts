c6c67dbd43ed14014322453b03fd203da35f1679
Maximo <maximors@gmail.com>
2026-04-05 03:15:30 -0300

    Finalizada clase Clínica

--- src/Clinica.ts
+++ src/Clinica.ts
@@ -94,13 +94,45 @@ export class Tratamiento implements IEntidad {
     }
  
     set nombre(valor: string) {
-        if (!valor.trim()) throw new Error("El nombre del tratamiento no puede estar vacío.");
+        if (!valor.trim()){
+            throw new Error("El nombre del tratamiento no puede estar vacío.");
+        }
         this._nombre = valor.trim();
     }
  
     set costo(valor: number) {
-        if (valor < 0) throw new Error("El costo no puede ser negativo.");
-        this._costo = valor;
+        if (valor < 0){ 
+            throw new Error("El costo no puede ser negativo.");
+        }
+            this._costo = valor;
+    }
+
+    avanzarEstado(): void {
+        const estado_2: EstadoTratamiento[] = ["pendiente", "en_progreso", "completado"];
+        const indiceActual = estado_2.indexOf(this._estado);
+        if (indiceActual === -1 || this._estado === "cancelado") {
+            throw new Error("No se puede avanzar el estado del tratamiento.");
+        }
+        if (indiceActual < estado_2.length - 1) {
+            const siguienteEstado = estado_2[indiceActual + 1];
+            if (!siguienteEstado) {
+                throw new Error("No se puede avanzar el estado del tratamiento.");
+            }
+            this._estado = siguienteEstado;
+            if (this._estado === "completado"){ 
+                this._fechaFin = new Date();
+            }
+        }
+    }
+ 
+    cancelar(): void {
+        if (this._estado == "completado") {
+            throw new Error("No se puede cancelar un tratamiento ya completado.");
+        }else if (this._estado == "en_progreso"){
+            throw new Error("No se puede cancelar un tratamiento en progreso.")
+        }
+        this._estado = "cancelado";
+        this._fechaFin = new Date();
     }
 }
 
@@ -127,6 +159,44 @@ export class FichaClinica implements IEntidad {
         return this._obraSocial;
     }
 
-
+    get tratamientos(): ReadonlyArray<Tratamiento>{ 
+        return this._tratamientos;
+    }
+    get antecedentes(): ReadonlyArray<AntecedenteMedico>{
+    return this._antecedentes; 
+}
+ 
+    set obraSocial(valor: string){ 
+        this._obraSocial = valor; 
+    }
+ 
+    agregarTratamiento(nuevoTratamiento: Tratamiento): void {
+        //Se busca el ID
+        for (const t of this._tratamientos) {
+            if (t.id === nuevoTratamiento.id) {
+                throw new Error("El tratamiento ya existe.");
+            }
+        }
+        //Se agrega el nuevo tratamiento
+        this._tratamientos = [...this._tratamientos, nuevoTratamiento];
+    }
+ 
+    agregarAntecedente(nuevoAntecedente: AntecedenteMedico): void {
+        for (const a of this._antecedentes) {
+            if (a.id === nuevoAntecedente.id) {
+                throw new Error(`El antecedente ya existe.`);
+            }
+        }
+        this._antecedentes = [...this._antecedentes, nuevoAntecedente];
+    }
+ 
+    buscarTratamiento(id: string): Tratamiento {
+        for (const t of this._tratamientos) {
+            if (t.id === id) {
+                return t;
+            }
+        }
+        throw new Error("No existe tal trataimento.")
+    }
 
 }
